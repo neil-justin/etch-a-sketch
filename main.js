2515f33236a2f45fbd879758cdffe2c3b6da08fa
Neil Justin <mallarineil9@gmail.com>
2022-07-20 20:16:16 +0800

    Simplify variables name

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function createGrid(size = 16) {
     const grid = document.querySelector('.grid');
-    const totalGridElements = size ** 2;
-    const elementSize = 500 / size; // where 500 is the width/height of the grid
+    const totalCells = size ** 2;
+    const cellSize = 500 / size; // where 500 is the width/height of the grid
 
-    for (let i = 0; i < totalGridElements; i++) {
-        const gridElement = document.createElement('div');
-        gridElement.setAttribute('style', `margin: 0; padding: 0; width: ${elementSize}px; \ 
-            height: ${elementSize}px;`);
-        grid.appendChild(gridElement);
+    for (let i = 0; i < totalCells; i++) {
+        const cell = document.createElement('div');
+        cell.setAttribute('style', `margin: 0; padding: 0; width: ${cellSize}px; \ 
+            height: ${cellSize}px;`);
+        grid.appendChild(cell);
     }
 
     return size;
