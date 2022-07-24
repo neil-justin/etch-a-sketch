15bb28d4375f085b91c4e620cacae18a4b2f4896
Neil Justin <mallarineil9@gmail.com>
2022-07-24 11:26:27 +0800

    Add Custom Color and Random Color button
    When the Random Color button is selected, each time the user clicks the canvas, a random color will be generated.

--- main.js
+++ main.js
@@ -42,3 +42,58 @@ resizeGridBtn.addEventListener('click', () => {
 
     createGrid(getGridSize());
 });
+
+let customPenColor;
+const colorPicker = document.querySelector('.color-picker');
+colorPicker.addEventListener('change', (e) => {
+    colorPicker.classList.add('active');
+    randomColorBtn.classList.remove('active');
+    return customPenColor = colorPicker.setAttribute('value', `${e.target.value}`);
+});
+
+const customColorBtn = document.querySelector('.custom-color-btn');
+customColorBtn.addEventListener('click', () => {
+    customColorBtn.classList.add('active');
+    randomColorBtn.classList.remove('active');
+    return customPenColor = colorPicker.value;
+});
+
+const randomColorBtn = document.querySelector('.random-color-btn');
+randomColorBtn.addEventListener('click', () => {
+    randomColorBtn.classList.add('active');
+    colorPicker.classList.remove('active');
+    customColorBtn.classList.remove('active');
+});
+
+let randomPenColor;
+function randomRgb(maxValue) {
+    let stringRgb = '';
+
+    for (let i = 0; i < 3; i++) {
+        if (i < 2) {
+            stringRgb += Math.floor(Math.random() * maxValue) + ", ";
+        } else {
+            stringRgb += Math.floor(Math.random() * maxValue);
+        }
+    }
+
+    const intArrayRgb = stringRgb.split(' ').map(element => parseInt(element));
+    return randomPenColor = intArrayRgb;
+}
+
+const cells = document.querySelectorAll('.cell');
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+        if (colorPicker.classList.contains('active') ||
+            customColorBtn.classList.contains('active')) {
+            cell.style.backgroundColor = `${customPenColor}`;
+        } else if (randomColorBtn.classList.contains('active')) {
+            randomRgb(255);
+            const r = randomPenColor[0];
+            const g = randomPenColor[1];
+            const b = randomPenColor[2];
+            cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        }
+    });
+});
+
